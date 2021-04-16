6acea1c590d4695f9ac772f47af2f2b562bb00f3
Hoàng Lê <72511303+hoangdung2001@users.noreply.github.com>
2021-04-16 22:07:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function findOppositeNumber(n,inputNumber){
-    if(inputNumber < n/2){
-        return inputNumber + n/2;
-    }
-    else{
-        if(inputNumber !== n/2){
-            return inputNumber - n/2;
-        }
-        else return 0;
-    }
-}
-console.log(findOppositeNumber(10,6));
+var a = document.getElementById("kqsx").innerHTML = Math.floor(Math.random() * 10)
