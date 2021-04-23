ad1025d058319ffae4cc12b18e12405223c2eec6
Hoàng Lê <72511303+hoangdung2001@users.noreply.github.com>
2021-04-23 22:49:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var a = document.getElementById("kqsx").innerHTML = Math.floor(Math.random() * 10)
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let inputNumber = document.getElementById("input");
+let turn = 3;
+
+function spin() {
+  let a = Number(inputNumber.value);
+  if (Number(inputNumber.value)) {
+    if (a == randomNumber) {
+      alert("congratulations, you guess correct");
+    } else {
+      turn--;
+      if (a != randomNumber) {
+        alert("You made a mistake and invite you to guess again" + "( You have " + turn + " turn left )");
+      }
+    }
+  } else {
+    alert("Sorry you are out of turn");
+  }
+  if (turn == 0) {
+    alert("Sorry you lost");
+  }
+}
